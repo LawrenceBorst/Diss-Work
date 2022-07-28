4afe608da968bfa3a7f587e7e1f40571dd9e8f4d
Lawrence B <70023170+LawrenceBorst@users.noreply.github.com>
2022-07-28 13:41:07 +0100

    Update GaussianOperator.py

--- GaussianOperator.py
+++ GaussianOperator.py
@@ -2,42 +2,20 @@
 """
 import numpy as np
 import GaussianState as gs
-
+import Conversions as conv
 
 class GaussianOperator:
     """
     Class for implementing state-space transformations and obtaining their matrix representations
 
     ...
-
-    Attributes
-    ----------
-    basis : str
-        Either "pos_mom" or "create_annihilate"
-
     Methods
     -------
     displace(gs_in: gs.GaussianState, alpha: np.ndarray) -> gs.GaussianState
-
-
     """
 
-    def __init__(self, basis: str):
-        """
-        Parameters
-        ----------
-        basis : str
-            Either "pos_mom" or "create_annihilate" depending on the desired basis
-
-        Raises
-        ------
-        Exception
-            If the basis string is not a valid basis
-        """
-
-        if basis not in ("pos_mom", "create_annihilate"):
-            raise Exception("Basis not valid")
-        self.basis = basis
+    def __init__(self):
+        return
 
     def displace(self, gs_in: gs.GaussianState, alpha: np.ndarray) -> gs.GaussianState:
         """Returns the displaced Gaussian state when applying displacement parameter a to it
@@ -54,17 +32,33 @@ class GaussianOperator:
         gs.GaussianState
             The transformed Gaussian state
         """
-        n_out = gs_in.n
-        x_out = gs_in.get_mu_x() + np.sqrt(2) * np.reshape(np.real(alpha), (-1, 1))
-        p_out = gs_in.get_mu_p() + np.sqrt(2) * np.reshape(np.imag(alpha), (-1, 1))
 
-        mu_out = np.empty((x_out.size + p_out.size, 1))
-        mu_out[0::2] = x_out
-        mu_out[1::2] = p_out
+        if gs_in.basis == "2BlockCA" or gs_in.basis == "2BlockXP":
+            n_out = gs_in.n
+            x_out = gs_in.get_mu_x() + np.sqrt(2) * np.reshape(np.real(alpha), (-1, 1))
+            p_out = gs_in.get_mu_p() + np.sqrt(2) * np.reshape(np.imag(alpha), (-1, 1))
 
-        sigma_out = gs_in.sigma
+            mu_out = np.empty((x_out.size + p_out.size, 1))
+            mu_out[0::2] = x_out
+            mu_out[1::2] = p_out
 
-        return gs.GaussianState(n_out, mu_out, sigma_out)
+            sigma_out = gs_in.sigma
+
+            return gs.GaussianState(n_out, mu_out, sigma_out, gs_in.basis)
+
+        if gs_in.basis == "nBlockCA" or gs_in.basis == "nBlockXP":
+            n_out = gs_in.n
+            x_out = gs_in.get_mu_x() + np.sqrt(2) * np.reshape(np.real(alpha), (-1, 1))
+            p_out = gs_in.get_mu_p() + np.sqrt(2) * np.reshape(np.imag(alpha), (-1, 1))
+
+            mu_out = np.empty((x_out.size + p_out.size, 1))
+            mu_out[:n_out:1] = x_out
+            mu_out[n_out::1] = p_out
+
+            sigma_out = gs_in.sigma
+
+            return gs.GaussianState(n_out, mu_out, sigma_out, gs_in.basis)
+
 
     def squeeze(self, gs_in: gs.GaussianState, t: np.ndarray) -> gs.GaussianState:
         """Returns the squeezed Gaussian state when applying squeezing (with parameters t)
@@ -84,15 +78,29 @@ class GaussianOperator:
         S = np.empty((0, 0))
 
         for r in t:
-            s = np.array([[np.exp(-r), 0], [0, np.exp(r)]])
+            s = np.array([[np.exp(-r), 0],
+                          [0, np.exp(r)]])
             S = np.block([[S, np.zeros((np.shape(S)[0], 2))],
-                                       [np.zeros((2, np.shape(S)[1])), s]])
+                        [np.zeros((2, np.shape(S)[1])), s]])
+
+        np.set_printoptions(precision=2)    # TODO REMOVE THIS
+
+        # transform S dependent on the basis
+        n = gs_in.n
+        if gs_in.basis == "nBlockXP":
+            pass
+        elif gs_in.basis == "2BlockXP":
+            S = conv.symp_form_n_to_2(n) @ S @ conv.symp_form_2_to_n(n)
+        elif gs_in.basis == "nBlockCA":
+            S = conv.xp_to_ca(n) @ S @ conv.ca_to_xp(n)
+        elif gs_in.basis == "2BlockCA":
+            S = conv.symp_form_n_to_2(n)@ conv.xp_to_ca(n) @ S @ conv.ca_to_xp(n) @ conv.symp_form_2_to_n(n)
 
         n_out = gs_in.n
         mu_out = S @ gs_in.mu
         sigma_out = S @ gs_in.sigma @ S.T
 
-        return gs.GaussianState(n_out, mu_out, sigma_out)
+        return gs.GaussianState(n_out, mu_out, sigma_out, gs_in.basis)
 
     def interferometer(self, gs_in: gs.GaussianState, U: np.ndarray) -> gs.GaussianState:
         """Returns the Gaussian state transformed by a linear interferometer
@@ -115,10 +123,11 @@ class GaussianOperator:
 
         Re_U = np.real(U)
         Im_U = np.imag(U)
-        S = np.block([[Re_U, -Im_U], [Im_U, Re_U]])
+        S = np.block([[Re_U, -Im_U],
+                      [Im_U, Re_U]])
 
         n_out = gs_in.n
         mu_out = S @ gs_in.mu
         sigma_out = S @ gs_in.sigma @ S.T
 
-        return gs.GaussianState(n_out, mu_out, sigma_out)
+        return gs.GaussianState(n_out, mu_out, sigma_out, gs_in.basis)
